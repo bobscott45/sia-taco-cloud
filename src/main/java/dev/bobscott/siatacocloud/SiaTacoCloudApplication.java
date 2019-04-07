4961ef5e5ca79736ce729dd5a709c53f2253e141
Bob Scott <bob@rwscott.co.uk>
2019-04-07 10:08:46 +0100

    Initial commit

---
+++ src/main/java/dev/bobscott/siatacocloud/SiaTacoCloudApplication.java
@@ -0,0 +1,13 @@
+package dev.bobscott.siatacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaTacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiaTacoCloudApplication.class, args);
+    }
+
+}
